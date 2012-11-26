41ff7fd897ebf6dd66f9e42451afd3b3eb9b10c0
Kristian Meier <m.kristian@web.de>
2012-11-26 21:52:46 +0530

    obsolete

--- app/models/remote_application.rb
+++
@@ -1,6 +0,0 @@
-class RemoteApplication < ActiveResource::Base
-  self.site = Translations::Application.config.remote_service_url
-  self.headers['X-SERVICE-TOKEN'] = Translations::Application.config.remote_service_token
-
-  self.element_name = "application"
-end
