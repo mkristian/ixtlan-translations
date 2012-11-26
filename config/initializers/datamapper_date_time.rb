dbb772a70a2677a15224b83fa7447910f10b08da
Kristian Meier <m.kristian@web.de>
2012-11-26 21:54:14 +0530

    fix timestamp - once again

--- config/initializers/datamapper_date_time.rb
+++ config/initializers/datamapper_date_time.rb
@@ -1,6 +1,8 @@
 require 'date'
-SECONDS_IN_DAY = 60 * 60 * 24
-NANOSECONDS_IN_DAY = SECONDS_IN_DAY * 1000 * 1000 * 1000
+
+# use UTC everywhere
+# keep default format to bedatabase compatible, i.e. no millis or nanos
+
 class DateTime
 
   def self.now
@@ -11,14 +13,10 @@ class DateTime
     if arg && ! arg.is_a?(Symbol)
       super
     elsif utc?
-      strftime('%Y-%m-%dT%H:%M:%S.') + ("%06d" % (sec_fraction / NANOSECONDS_IN_DAY / 1000))
+      strftime('%Y-%m-%dT%H:%M:%S')
     else
       utc.to_s
     end
   end
 
-#  def self.parse(str)
-#    result = super (str+"+0:00").sub(/ /, 'T')
-#    result
-#  end
 end
