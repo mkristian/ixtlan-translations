259bb4a1da76493afa8b8c31eb8689cd654d6769
Christian Meier <m.kristian@web.de>
2013-01-13 12:10:11 +0530

    allow secret token via password file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -23,4 +23,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Translations::Application.config.secret_token = 'b676188496896b7ff2e5b89444204a2ea5bf2d50d40650201a779f60e3ef0cad0c20354dfe2c085b58db48ae54b11296dd135a0baa574ac8d120151b7debcf33'
+Translations::Application.config.secret_token = Ixtlan::Passwords.get( :secret_token, SecureRandom.hex( 64 ) )
