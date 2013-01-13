feefc01f7077975a82f9d19a1298e0d9126190b5
Christian Meier <m.kristian@web.de>
2013-01-13 14:08:18 +0530

    get the password file loaded

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -17,35 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with ixtlan_translations.  If not, see <http://www.gnu.org/licenses/>.
 #
-require 'yaml'
-require 'erb'
-module Ixtlan
-  class Configurator
+require 'ixtlan/passwords'
 
-    def self.symbolize_keys(h)
-      result = {}
-
-      h.each do |k, v|
-        v = ' ' if v.nil?
-        if v.is_a?(Hash)
-          result[k.to_sym] = symbolize_keys(v) unless v.size == 0
-        else
-          result[k.to_sym] = v unless k.to_sym == v.to_sym
-        end
-      end
-
-      result
-    end
-
-    def self.load(file)
-      if File.exists?(file)
-        symbolize_keys(YAML::load(ERB.new(IO.read(file)).result))
-      else
-        f = file.sub /#{File.dirname(File.dirname(__FILE__)) }\/?/, ''
-        warn "no file #{f} to load"
-      end
-    end
-  end
-end
-
-CONFIG = Ixtlan::Configurator.load(File.join(File.dirname(__FILE__), 'passwords.yml')) || {}
+CONFIG = Ixtlan::Passwords.config( File.join( File.dirname( __FILE__ ), 'passwords.yml' ) )
